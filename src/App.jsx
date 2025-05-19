34337e8e71020500ffab8de3b6fc5bb029b6e897
Disha <dvshetiya@gmail.com>
2025-05-18 22:01:19 -0500

    Initial working Tailwind + Vite setup

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import "./index.css";
+function App() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-blue-100">
+      <h1 className="text-4xl font-bold text-red-700">Hello Tailwind</h1>
+    </div>
+  );
+}
+
+export default App;
