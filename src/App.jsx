608a73b498b58ef9bdf6409a25faf2126532e0e5
Disha <dvshetiya@gmail.com>
2025-05-18 22:11:31 -0500

    fix: change header text color from red to blue

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import "./index.css";
 function App() {
   return (
     <div className="min-h-screen flex items-center justify-center bg-blue-100">
-      <h1 className="text-4xl font-bold text-red-700">Hello Tailwind</h1>
+      <h1 className="text-4xl font-bold text-blue-700">Hello Tailwind</h1>
     </div>
   );
 }
